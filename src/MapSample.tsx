eab905cb70c47e33b015f6b4a6227b18f1a0a1cf
pocari <caffelattenonsugar@gmail.com>
2020-05-10 15:11:32 +0900

    地球描く

---
+++ src/MapSample.tsx
@@ -0,0 +1,130 @@
+import React, { Component, FC, useEffect, useState, useRef } from 'react'
+import * as d3 from 'd3'
+import * as topojson from 'topojson'
+import  { Topology } from 'topojson-specification'
+import { Feature } from 'geojson'
+
+const LOADING = 'Loading' as const
+const SUCCESS = 'Success' as const
+const FAILURE = 'Failure' as const
+
+type Status = typeof LOADING | typeof SUCCESS | typeof FAILURE
+
+const styles = {
+  svg: {
+    width: '100vw',
+    height: '100vw'
+  }
+}
+
+interface MapFile {
+  url: string;
+  objectsname: string
+}
+
+const MapSample: FC<MapFile> = ({
+  url,
+  objectsname,
+}) => {
+  const [ topology, setTopology] = useState<Topology | null>(null)
+  const [ status, setStatus ] = useState<Status>(LOADING)
+  const svg = useRef<SVGSVGElement>(null)
+  useEffect(() => {
+     (async () => {
+       console.log("load json")
+       try {
+         setStatus(LOADING)
+         setTopology(await d3.json<Topology>(url))
+         console.log("load json success")
+         setStatus(SUCCESS)
+       } catch (e) {
+         console.log("load json failed")
+         setStatus(FAILURE)
+       }
+     })()
+  }, [setStatus, setTopology])
+
+  useEffect(() => {
+    if (svg.current) {
+      console.log('didupdate')
+      const svgElem = d3.select<SVGSVGElement, Feature[]>(svg.current)
+      console.log(`svgElem: ${typeof svgElem}`)
+      draw(svgElem)
+    }
+  })
+
+  const getProjection = (): d3.GeoProjection => {
+    let width = 0
+    let height = 0
+    if (svg.current) {
+      width = svg.current.clientWidth
+      height = svg.current.clientHeight
+    }
+    return d3.geoOrthographic()
+      .scale(300)
+      .translate([width / 2, height / 2])
+  }
+
+  const draw = (svg: d3.Selection<SVGSVGElement, Feature[], null, undefined>) => {
+    console.log("draw start")
+    if (status !== SUCCESS) {
+      return
+    }
+    const mapData = topology
+    if (!mapData) return
+
+    const projection = getProjection()
+    const pathGenerator = d3.geoPath()
+      .projection(projection)
+
+    console.log('objectsname')
+    console.log(objectsname)
+    console.log('mapData.objects[objectsname]')
+    console.log(mapData.objects)
+
+    console.log("-------- keys")
+    for (let key in mapData.objects) {
+      console.log(key)
+    }
+
+    const feature = topojson
+      .feature(mapData, mapData.objects[objectsname])
+    const features: Feature[] = (feature.type === "FeatureCollection") ?
+      feature.features : [feature]
+
+    const g = svg.append('g')
+    const item = g.selectAll('.item')
+      .data(features)
+
+    item.exit().remove()
+
+    item.enter()
+      .append('path')
+      .attr('class', 'shape item')
+      .attr('d', pathGenerator)
+      .style('fill', 'black')
+      .style('stroke', () => "white")
+      .style('stroke-width', () => 0.1)
+    console.log("draw end")
+  }
+
+  if (status === LOADING) {
+    return <div>Loading ... </div>
+  }
+
+  if (status === FAILURE) {
+    return <div>load json failed </div>
+  }
+
+  return (
+    <div>
+      <div>{url}</div>
+      <div>{objectsname}</div>
+      <svg style={styles.svg} ref={svg} />
+    </div>
+
+  )
+}
+
+
+export default MapSample
